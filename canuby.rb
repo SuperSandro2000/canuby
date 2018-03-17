4c2b37134c6b6bbda49dbd38d3e2dec38a99ceaa
Sandro Jäckel <sandro.jaeckel@gmail.com>
2018-03-17 12:34:49 +0100

    Started modularization

--- canuby.rb
+++ canuby.rb
@@ -1,96 +1,78 @@
+# Copyright (C) 2018 Sandro Jäckel.  All rights reserved.
+#
+# This file is part of Canuby.
+#
+# Canuby is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canuby is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Canuby.  If not, see <http://www.gnu.org/licenses/>.
+require_relative 'util'
+
+## Projects config
+Projects = {}
+Projects['googletest'] = {'url' => 'https://github.com/google/googletest', 'version' => '1.0.0'}
+Projects['dummy'] = {'url' => 'https://github.com/google/googletest', 'version' => '1.0.0'}
+Projects['dummy2'] = {'url' => 'https://github.com/google/googletest', 'version' => '1.0.0'}
+
+## build tools config
+ENV['vcvars'] ||= '"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"'
+ENV['rel_type'] ||= 'RelWithDebInfo'
 
 task default: :thirdparty
 
-ENV['vcvars'] ||= '"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"'
-
 desc 'Prepare 3rdparty dependencies'
-task thirdparty: 'thirdparty:googletest'
+task thirdparty: ['thirdparty:googletest', 'thirdparty:dummy']
 
-namespace :thirdparty do
-  def base_dir
-    '3rdparty'
-  end
+Projects.keys.each do |project|
+  namespace :thirdparty do
+    Projects[project]['path'] = File.join(base_dir, project)
 
-  def stage_dir
-    File.join(base_dir, 'lib')
-  end
+    desc "Prepare #{project}"
+    task "#{project}": "#{project}:staged"
 
-  task :init do
-    mkdir_p base_dir unless Dir.exist?(base_dir)
-  end
-
-  desc 'Prepare googletest'
-  task googletest: 'googletest:staged'
-
-  namespace :googletest do
-    def project_dir
-      File.join(base_dir, 'googletest')
-    end
-
-    def outputs
-      ['gtest.lib', 'gtest_main.lib']
-    end
-
-    def build_outputs
-      output_dir = File.join(project_dir, 'build', 'googlemock', 'gtest', 'RelWithDebInfo')
-      outputs.collect { |f| File.join(output_dir, f) }
-    end
-
-    def stage_outputs
-      outputs.collect { |f| File.join(stage_dir, f) }
-    end
-
-    def clean_stage
-      stage_outputs.each { |f| rm f if File.exist?(f) }
-    end
-
-    def do_clone
-      puts 'Cloning googletest...'
-      sh 'git clone https://github.com/google/googletest.git #{project_dir}'
-    end
-
-    def do_build
-      puts 'Building googletest...'
-      clean_stage
-      build_dir = File.join(project_dir, 'build')
-      mkdir_p build_dir
-      Dir.chdir(build_dir) do
-        sh "call #{ENV['vcvars']} && cmake .. && msbuild googletest-distribution.sln /p:Configuration=RelWithDebInfo /p:Platform=Win32 /v:m"
+    namespace project do
+      def outputs
+        ['gtest.lib', 'gtest_main.lib']
       end
-    end
 
-    def do_stage
-      puts 'Staging googletest...'
-      mkdir_p stage_dir
-      cp build_outputs, stage_dir
-    end
-
-    task cloned: :init do
-      do_clone unless File.exist?(project_dir)
-    end
-
-    task built: :cloned do
-      do_build unless build_outputs.all? { |f| File.exist?(f) }
-    end
-
-    task staged: :built do
-      do_stage unless stage_outputs.all? { |f| File.exist?(f) }
-    end
-
-    desc 'Pull upstream googletest changes'
-    task :pull do
-      Dir.chdir(project_dir) do
-        sh 'git pull'
+      def build_outputs(project)
+        output_dir = File.join(Projects[project]['path'], 'build', 'googlemock', 'gtest', ENV['rel_type'])
+        outputs.collect { |f| File.join(output_dir, f) }
       end
-    end
 
-    desc 'Build and stage googletest'
-    task build_stage: :cloned do
-      do_build
-      do_stage
-    end
+      task cloned: :init do
+        git_clone(Projects[project]['url'], project) unless File.exist?(Projects[project]['path'])
+      end
 
-    desc 'Pull, build and stage googletest'
-    task update: [:pull, :build_stage]
+      task built: :cloned do
+        msbuild('googletest') unless build_outputs('googletest').all? { |f| File.exist?(f) }
+      end
+
+      task staged: :built do
+        collect_stage('googletest') unless stage_outputs.all? { |f| File.exist?(f) }
+      end
+
+      desc 'Pull upstream googletest changes'
+      task :pull do
+        git_pull
+      end
+
+      desc 'Build and stage googletest'
+      task build_stage: :cloned do
+        msbuild('googletest')
+        collect_stage('googletest')
+      end
+
+      desc 'Pull, build and stage googletest'
+      task update: [:pull, :build_stage]
+    end
   end
 end
---
+++ util.rb
@@ -0,0 +1,69 @@
+# Copyright (C) 2018 Sandro Jäckel.  All rights reserved.
+#
+# This file is part of Canuby.
+#
+# Canuby is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canuby is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Canuby.  If not, see <http://www.gnu.org/licenses/>.
+
+## folders and files
+def base_dir
+  '3rdparty'
+end
+
+def stage_dir
+  File.join(base_dir, 'lib')
+end
+
+def stage_outputs
+  outputs.collect { |f| File.join(stage_dir, f) }
+end
+
+## external tools methods
+def git_clone(url, project)
+  puts "Cloning #{url}... to Projects[project]['path']"
+  sh "git clone #{url} #{Projects[project]['path']}"
+end
+
+def git_pull
+  Dir.chdir(Projects[project]['path'])
+  sh 'git pull'
+end
+
+def msbuild(project)
+  puts "Building #{project}..."
+  clean_stage
+  build_dir = File.join(Projects[project]['path'], 'build')
+  mkdir_p build_dir
+  Dir.chdir(build_dir) do
+    sh 'cmake ..'
+    sh "call #{ENV['vcvars']} && msbuild #{project}-distribution.sln /p:Configuration=#{ENV['reL_type']} /p:Platform=Win32 /v:m"
+  end
+end
+
+## stage
+def clean_stage
+  stage_outputs.each { |f| rm f if File.exist?(f) }
+end
+
+def collect_stage(project)
+  puts "Staging #{project}..."
+  mkdir_p stage_dir
+  cp build_outputs(project), stage_dir
+end
+
+## standard tasks
+namespace :thirdparty do
+  task :init do
+    mkdir_p base_dir unless Dir.exist?(base_dir)
+  end
+end
